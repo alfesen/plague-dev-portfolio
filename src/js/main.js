35d10b21269489b3b3484da63699d1f9e60d8a95
alfesen <89510380+alfesen@users.noreply.github.com>
2022-06-04 17:06:09 +0200

    Add content and language switching

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,10 @@ const mobileNav = document.querySelector('.nav__mobile')
 const mobileNavLinks = document.querySelectorAll('.nav__mobile-link')
 const accordion = document.getElementsByClassName('label')
 const year = document.querySelector('.footer-year')
-
+const polish = document.querySelectorAll('.pl')
+const english = document.querySelectorAll('.en')
 const DOM = document.body
+let selectedValue = 'en'
 
 const classNames = {
   activeSkills: 'active',
@@ -12,6 +14,50 @@ const classNames = {
   showNav: 'show-nav',
   stickyBody: 'sticky-body',
   activeChevron: 'active-chevron',
+}
+
+const updateLanguage = () => {
+  const mobileLang = document.getElementById('mobile-lang')
+  let mobileValue = mobileLang.value
+  const desktopLang = document.getElementById('desktop-lang')
+  let desktopValue = desktopLang.value
+  const mainSkills = document.querySelector('.main-skills')
+  const subsidarySkills = document.querySelector('.subsidary-skills')
+  const ambitions = document.querySelector('.ambitions')
+
+  if (document.body.clientWidth > 768 ) {
+    mobileValue = desktopValue
+  } else if (document.body.clientWidth <= 768) {
+    desktopValue = mobileValue
+  }
+
+  if (desktopValue === 'en') {
+    polish.forEach(item => {
+      item.style.display = 'none'
+    })
+
+    english.forEach(item => {
+      item.style.display = 'flex'
+    })
+
+    mainSkills.innerHTML = 'main technologies <i class="fas fa-chevron-down"></i>'
+    subsidarySkills.innerHTML = 'subsidary skills <i class="fas fa-chevron-down"></i>'
+    ambitions.innerHTML = 'closest ambitions <i class="fas fa-chevron-down"></i>'
+  } else if (desktopValue === 'pl') {
+    english.forEach(item => {
+      item.style.display = 'none'
+    })
+
+    polish.forEach(item => {
+      item.style.display = 'flex'
+    })
+
+    mainSkills.innerHTML = 'główne technologie <i class="fas fa-chevron-down"></i>'
+    subsidarySkills.innerHTML = 'pomocne umiejętności <i class="fas fa-chevron-down"></i>'
+    ambitions.innerHTML = 'najbliższe ambicje <i class="fas fa-chevron-down"></i>'
+  } 
+
+  hideNav()
 }
 
 const handleNav = () => {
@@ -57,10 +103,10 @@ new Glide('.glide', {
 }).mount()
 
 const handleYear = () => {
-  const newYear = new Date().getFullYear();
-  year.textContent = newYear;
+  const newYear = new Date().getFullYear()
+  year.textContent = newYear
 }
 
 burgerBtn.addEventListener('click', handleNav)
 handleAccorderon()
-handleYear();
+handleYear()
